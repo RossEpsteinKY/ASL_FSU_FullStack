0af8b33e40fce65b46396a9a2176dbb079eeb838
RossEpsteinKY <31447738+RossEpsteinKY@users.noreply.github.com>
2022-08-27 22:32:42 -0400

    added wrapper for loading in app.tsx

--- asl_frontend/src/App.tsx
+++ asl_frontend/src/App.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import {Fragment, useState} from 'react'
 import logo from './logo.svg';
 import './App.css';
 import Dashboard from "./pages/dashboard";
 
 function App() {
+    const [auth, setAuth] = useState(null);
   return (
     <div className="App">
-      <Dashboard />
+        {auth === null && (
+
+        <h1>loading</h1>
+        )}
+        {auth && (
+            <Dashboard />
+        )}
+
+
+
     </div>
   );
 }
